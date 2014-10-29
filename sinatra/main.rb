4a84b7c374cb319be5ebf110b3674d4ace8e3690
mayumiishiyama <mayumi@ishiyamamayubi-no-MacBook-Pro-3.local>
2014-10-29 18:52:17 +0900

    投票してきたアカウントをログに残せるように修正

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 
+log = File.open(File.expand_path("#{settings.root}/log/#{Time.now.strftime '%Y%m%d'}.log", __FILE__), "a")
+
 get '/vote' do
   @title = "投票"
   erb :vote
 end
 
 post '/result' do
+  log.puts "#{params[:data]}"
   @title = "投票結果"
   erb :result
 end
