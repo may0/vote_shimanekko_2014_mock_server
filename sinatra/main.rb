cc05a9a1b7d81df34381c2aff47204667a318336
mayumiishiyama <mayumi@ishiyamamayubi-no-MacBook-Pro-3.local>
2014-11-05 13:11:28 +0900

    複数回の投票を拒否するように修正

--- sinatra/main.rb
+++ sinatra/main.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 require './user.rb'
+require './vote.rb'
 
-log = File.open(File.expand_path("#{settings.root}/log/#{Time.now.strftime '%Y%m%d'}.log", __FILE__), "a")
+File.open(File.expand_path("#{settings.root}/log/#{Time.now.strftime '%Y%m%d'}.log", __FILE__), "a")
 
 get '/vote' do
   @title = "投票"
@@ -10,10 +11,18 @@ end
 
 post '/result' do
   user = User.new(email:params[:data][:Member][:email], password:params[:data][:Member][:password])
+  vote = Vote.new(email:params[:data][:Member][:email], password:params[:data][:Member][:password])
 
-  if user.valid?
-    log.puts "#{params[:data][:Member][:email]}:#{params[:data][:Member][:password]}"
+  if vote.valid?
     @title = "投票結果"
+    @description = "本日は既に投票済みです"
+    erb :result
+  elsif user.valid?
+    File.open(File.expand_path("#{settings.root}/log/#{Time.now.strftime '%Y%m%d'}.log", __FILE__), "a") do |log|
+      log.puts "#{params[:data][:Member][:email]}:#{params[:data][:Member][:password]}"
+    end
+    @title = "投票結果"
+    @description = "投票完了"
     erb :result
   else
     erb :error
